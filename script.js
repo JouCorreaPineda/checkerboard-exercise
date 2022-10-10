d693e85226631bdcde06538100c9c37b6f839f77
JouCorreaPineda <110700857+JouCorreaPineda@users.noreply.github.com>
2022-10-09 21:09:38 -0400

    added color gradient to each square

--- script.js
+++ script.js
@@ -1,55 +1,98 @@
-//create a variable that represents the body element present in our html file.
-const body = document. querySelector('body');
 
-//create a div element to place inside of our body element
-const board = document.createElement('div');
-    //add that div element into our body element
-    body.appendChild(board);
-    //convert that element into a grid with 9 columns of width 11.1%
-    board.style.display = 'grid';
-    board.style.gridTemplateColumns = "11.1% 11.1% 11.1% 11.1% 11.1% 11.1% 11.1% 11.1% 11.1%";
+// //Method #1
+
+// const body = document. querySelector('body');
+
+// const board = document.createElement('div');
+// board.style.display = 'grid';
+// board.style.gridTemplateColumns = "11.1% 11.1% 11.1% 11.1% 11.1% 11.1% 11.1% 11.1% 11.1%";
+// body.appendChild(board);
     
 
-//This function will create everything within the board
-function ColumnCreator(){
-    //for every column
-    for(var i=0;i<9;i++){
-        //create the column div element
-        const Column = document.createElement('div');
-        //add it to the board div
-        board.appendChild(Column);
-        //if the column number is even
+
+// function ColumnCreator(){
+//     for(var i=0;i<9;i++){
+//         const Column = document.createElement('div');
+//         board.appendChild(Column);
+//         Column.style.height="100%"
+//         if(i%2==0){
+//             for(var j=0;j<9;j++){
+//                 const square = document.createElement('div');
+//                 Column.appendChild(square);
+//                 square.style.height = "11.1%";
+//                     if(j%2==0){
+//                         square.style.backgroundColor = 'red';
+//                     }else{
+//                         square.style.backgroundColor = 'black';
+//                     }
+//             }
+//         }
+//         else{
+//             for(var j=0;j<9;j++){
+//                 const square = document.createElement('div');
+//                 Column.appendChild(square);
+//                 square.style.height = "11.1%";
+//                     if(j%2==0){
+//                         square.style.backgroundColor = 'black';
+//                     }else{
+//                         square.style.backgroundColor = 'red';
+//                     }
+//             }
+//         }
+//     }
+//     return square;
+// }
+
+// ColumnCreator();
+
+// //QUESTIONS//
+// //why dont my percentage symbols work on lines 24 and 34:
+// //square.style.height = "11.1%";
+// //square.style.height = "11.1%";
+
+
+
+//- - - - - - - - - - - - - - - - - - - - - - -- - - - - - - - - -
+
+//Method #2
+
+const body = document. querySelector('body');
+const squareContainer = document.createElement('div');
+body.appendChild(squareContainer);
+
+function squareCreator(){
+    for(var i=0;i<81;i++){
+        //var colors = ['red','blue','green','yellow','orange','purple','lightblue','white'];
+        var color1 = Math.floor(Math.random() * 255);
+        var color2 = Math.floor(Math.random() * 255);
+        var color3 = Math.floor(Math.random() * 255);
+        var color1neg = (255 - color1).toString()
+        var color2neg = (255 - color2).toString()
+        var color3neg = (255 - color3).toString()
+        var rgbneg = "rgb("+color1neg+","+color2neg+","+color3neg+")"
+
         if(i%2==0){
-            //if the 
-            for(var j=0;j<9;j++){
-                const square = document.createElement('div');
-                Column.appendChild(square);
-                square.style.height = "11.1%";
-                    if(j%2==0){
-                        square.style.backgroundColor = 'red';
-                    }else{
-                        square.style.backgroundColor = 'black';
-                    }
-
-            }
-        }else{
-            for(var j=0;j<9;j++){
-                const square = document.createElement('div');
-                Column.appendChild(square);
-                square.style.height = "11.1%";
-                    if(j%2==0){
-                        square.style.backgroundColor = 'black';
-                    }else{
-                        square.style.backgroundColor = 'red';
-                    }
-            }
-
+            var square = document.createElement('div');
+            square.style.float="left";
+            square.style.width = "11.1%";
+            square.style.height="11.1%";
+            square.style.paddingBottom="11.1%"
+            square.style.backgroundColor = "rgb("+color1.toString()+","+color2.toString()+","+color3.toString()+")";
+            square.style.backgroundImage = 'linear-gradient(to bottom,'+square.style.backgroundColor+','+rgbneg+')';
+            squareContainer.appendChild(square);
         }
-        
+        else{
+            var square = document.createElement('div');
+            square.style.float="left";
+            square.style.width = "11.1%";
+            square.style.height="11.1%";
+            square.style.paddingBottom="11.1%"
+            square.style.backgroundColor = "rgb("+color1.toString()+","+color2.toString()+","+color3.toString()+")";
+            square.style.backgroundImage = 'linear-gradient(to bottom,'+square.style.backgroundColor+','+rgbneg+')';
+            squareContainer.appendChild(square); 
+        }
     }
-    return square;
+return square;
 }
 
-ColumnCreator();
-
-
+squareCreator();
